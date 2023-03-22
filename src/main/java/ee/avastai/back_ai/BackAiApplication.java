67235590b283ba8567ccc91214cbddaec2a4494b
Valter Aro <valteraro@gmail.com>
2023-03-22 14:43:48 +0200

    Initial commit

---
+++ src/main/java/ee/avastai/back_ai/BackAiApplication.java
@@ -0,0 +1,13 @@
+package ee.avastai.back_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAiApplication.class, args);
+	}
+
+}
